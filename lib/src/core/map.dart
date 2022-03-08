37b9b7f048cab2fbf52c802bb24daeaadcba3564
Andres Atehortua <andres.atehortua@zemoga.com>
2022-03-08 16:28:49 -0500

    Remove redundant `Map` extension methods.

--- lib/src/core/map.dart
+++ lib/src/core/map.dart
@@ -1,29 +1,7 @@
 extension MapExtension<K, V> on Map<K, V> {
-  Iterable<MapEntry<K, V>> where(
+  Map<K, V> where(
     bool Function(MapEntry<K, V> entry) test,
   ) {
-    return entries.where(test);
-  }
-
-  MapEntry<K, V> firstWhere(
-    bool Function(MapEntry<K, V> entry) test, {
-    MapEntry<K, V> Function()? orElse,
-  }) {
-    return entries.firstWhere(
-      test,
-      orElse: orElse,
-    );
-  }
-
-  V firstValueWhere(
-    bool Function(K key) test, {
-    V Function()? orElse,
-  }) {
-    try {
-      return firstWhere((entry) => test(entry.key)).value;
-    } catch (e) {
-      if (orElse != null) return orElse();
-      rethrow;
-    }
+    return Map.fromEntries(entries.where(test));
   }
 }
