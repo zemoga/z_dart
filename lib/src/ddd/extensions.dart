e9c2465a28f1af196a5102e9d1329bdebe714ac1
Andres Atehortua <andres.atehortua@zemoga.com>
2022-03-09 18:06:07 -0500

    Specialized collection cache for entities.

--- lib/src/ddd/extensions.dart
+++ lib/src/ddd/extensions.dart
@@ -2,6 +2,39 @@ import '../../io.dart';
 import 'entity.dart';
 import 'value_object.dart';
 
+///
+mixin EntityCollectionCacheMixin<T extends Entity>
+    implements CollectionCacheMixin<T> {
+  Future<bool> containsEntity(T entity) {
+    return contains(entity.id.toString());
+  }
+
+  Future<void> addEntity(T entity) {
+    return add(entity.id.toString(), entity);
+  }
+
+  Future<void> addAllEntities(List<T> otherEntities) {
+    return addAll(otherEntities.toMap());
+  }
+
+  Future<void> replaceAllEntities(List<T> otherEntities) {
+    return replaceAll(otherEntities.toMap());
+  }
+
+  Future<void> removeEntity(T entity) {
+    return remove(entity.id.toString());
+  }
+}
+
+///
+class EntityCollectionCache<T extends Entity> extends CollectionCache<T>
+    with EntityCollectionCacheMixin<T> {
+  EntityCollectionCache() : super();
+
+  EntityCollectionCache.from(Iterable<T> entities)
+      : super.from(entities.toMap());
+}
+
 extension LegacyCacheExt<K extends ValueObject, V extends Entity<K>>
     on LegacyCache<K, V> {
   Future<void> putEntity(V entity) => this.put(entity.id, entity);
