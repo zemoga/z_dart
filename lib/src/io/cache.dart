633e56a677103c71406fc730d2e251ce5179fffc
Andres Atehortua <andres.atehortua@zemoga.com>
2022-03-09 13:59:35 -0500

    Create a more general-use `Cache` base class.

--- lib/src/io/cache.dart
+++ lib/src/io/cache.dart
@@ -1,3 +1,5 @@
+import 'package:z_dart/async.dart';
+
 ///
 class CachePolicy {
   final Duration expireAfter;
@@ -13,4 +15,32 @@ class CachePolicy {
   void validate() => _validationTime = DateTime.now();
 
   void invalidate() => _validationTime = DateTime.fromMillisecondsSinceEpoch(0);
+}
+
+///
+class Cache<T> {
+  Cache(T initialData) {
+    emit(initialData);
+  }
+
+  // This Subject is intended to be broadcasting while
+  // the app is active so new events are propagated in a reactive way
+  //
+  // This analyzer warning is safe to ignore.
+  // ignore: close_sinks
+  final Subject<T> _subject = BehaviorSubject<T>();
+
+  late T _data;
+
+  /// The current [data].
+  T get data => _data;
+
+  /// The current value stream.
+  Stream<T> get stream => _subject.stream;
+
+  /// Updates the [data] to the provided [data].
+  void emit(T data) {
+    _data = data;
+    _subject.add(data);
+  }
 }
