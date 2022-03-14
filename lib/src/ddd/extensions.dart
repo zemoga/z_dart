ffe7bcc34b83bdd69ff6c91bb1460c94715218b6
Andres Atehortua <andres.atehortua@zemoga.com>
2022-03-14 09:51:12 -0500

    Remove deprecated entity-cache logic.

--- lib/src/ddd/extensions.dart
+++ lib/src/ddd/extensions.dart
@@ -1,40 +1,5 @@
 part of z.dart.ddd;
 
-///
-@Deprecated('Use IdentifiableCollectionCacheMixin instead.')
-mixin EntityCollectionCacheMixin<T extends Entity>
-    implements CollectionCacheMixin<T> {
-  Future<bool> containsEntity(T entity) {
-    return contains(entity.id.toString());
-  }
-
-  Future<void> addEntity(T entity) {
-    return add(entity.id.toString(), entity);
-  }
-
-  Future<void> addAllEntities(List<T> otherEntities) {
-    return addAll(otherEntities.toMap());
-  }
-
-  Future<void> replaceAllEntities(List<T> otherEntities) {
-    return replaceAll(otherEntities.toMap());
-  }
-
-  Future<void> removeEntity(T entity) {
-    return remove(entity.id.toString());
-  }
-}
-
-///
-@Deprecated('Use IdentifiableCollectionCache instead.')
-class EntityCollectionCache<T extends Entity> extends CollectionCache<T>
-    with EntityCollectionCacheMixin<T> {
-  EntityCollectionCache() : super();
-
-  EntityCollectionCache.from(Iterable<T> entities)
-      : super.from(entities.toMap());
-}
-
 @Deprecated('This will be removed in a future version')
 extension LegacyCacheExt<K extends ValueObject, V extends Entity<K>>
     on LegacyCache<K, V> {
