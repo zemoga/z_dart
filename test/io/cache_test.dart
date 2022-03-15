5732000983c808b95a26c818536762602f43371d
Andres Atehortua <andres.atehortua@zemoga.com>
2022-03-15 11:58:28 -0500

    Add '`cache` unit-tests.

---
+++ test/io/cache_test.dart
@@ -0,0 +1,95 @@
+import 'package:test/test.dart';
+import 'package:z_dart/io.dart';
+
+void main() {
+  group('Empty CollectionCache', () {
+    late CollectionCache collectionCache;
+    setUp(() {
+      collectionCache = CollectionCache();
+    });
+    tearDown(() {
+      collectionCache.clear();
+    });
+    test('.add() adds a data entry to cache', () async {
+      final key = 'key';
+      final value = 45;
+
+      await collectionCache.add(key, value);
+
+      final cacheValues = await collectionCache.values.first;
+      expect(cacheValues, isNotEmpty);
+      expect(cacheValues.first, equals(value));
+    });
+    test('.addAll() adds a data map to cache', () async {
+      final other = {'keyA': 25, 'keyB': 50, 'keyC': 75, 'keyD': 100};
+
+      await collectionCache.addAll(other);
+
+      final cacheValues = await collectionCache.values.first;
+      expect(cacheValues, isNotEmpty);
+      expect(cacheValues, hasLength(4));
+      expect(cacheValues, equals(other.values));
+    });
+    test('.replaceAll() replaces all cache data with a new data map', () async {
+      final oldData = {'key': 0};
+
+      await collectionCache.addAll(oldData);
+
+      final oldCacheValues = await collectionCache.values.first;
+      expect(oldCacheValues, isNotEmpty);
+      expect(oldCacheValues, hasLength(1));
+      expect(oldCacheValues, equals(oldData.values));
+
+      final newData = {'keyA': 25, 'keyB': 50, 'keyC': 75, 'keyD': 100};
+
+      await collectionCache.replaceAll(newData);
+
+      final cacheValues = await collectionCache.values.first;
+      expect(cacheValues, isNotEmpty);
+      expect(cacheValues, hasLength(4));
+      expect(cacheValues, equals(newData.values));
+    });
+  });
+  group('Prepopulated CollectionCache', () {
+    late CollectionCache collectionCache;
+    setUp(() {
+      final data = {'dftlKey': 100};
+      collectionCache = CollectionCache.from(data);
+    });
+    tearDown(() {
+      collectionCache.clear();
+    });
+    test('.add() adds a value to cache', () async {
+      final key = 'key';
+      final value = 30;
+
+      await collectionCache.add(key, value);
+
+      final cacheValues = await collectionCache.values.first;
+      expect(cacheValues, isNotEmpty);
+      expect(cacheValues, hasLength(2));
+      expect(cacheValues[1], equals(value));
+    });
+  });
+  group('Prepopulated CollectionCache with immutable data', () {
+    late CollectionCache collectionCache;
+    setUp(() {
+      final data = Map<String, int>.unmodifiable({'dftlKey': 100});
+      collectionCache = CollectionCache.from(data);
+    });
+    tearDown(() {
+      collectionCache.clear();
+    });
+    test('.add() adds a value to cache', () async {
+      final key = 'key';
+      final value = 30;
+
+      await collectionCache.add(key, value);
+
+      final cacheValues = await collectionCache.values.first;
+      expect(cacheValues, isNotEmpty);
+      expect(cacheValues, hasLength(2));
+      expect(cacheValues[1], equals(value));
+    });
+  });
+}
