c13adaa7e3002d5b985b58e22e9f4516f9366471
Andres Atehortua <andres.atehortua@zemoga.com>
2022-04-22 09:57:36 -0500

    Minor code grouping optimization.

--- lib/src/ddd/entity.dart
+++ lib/src/ddd/entity.dart
@@ -1,5 +1,33 @@
 part of z.dart.ddd;
 
+abstract class EntityId extends SingleValueObject<String>
+    implements Comparable<EntityId> {
+  const EntityId(String value) : super(value);
+
+  bool get isValid => value.isNotEmpty;
+
+  /// Check this ID is of numeric type, meaning, only
+  /// digits compose the ID.
+  ///
+  /// Only positive integer values are considered a
+  /// valid numeric ID.
+  bool get isNumeric => !(int.tryParse(value) ?? -1).isNegative;
+
+  /// Gets the numeric representation of this ID.
+  ///
+  /// If [value] is not of numeric type, throws a [FormatException].
+  int get numericValue => isNumeric
+      ? int.parse(value)
+      : throw FormatException('ID is not a numeric value: $value');
+
+  @override
+  String toString() => value;
+
+  @override
+  int compareTo(EntityId other) => value.compareTo(other.value);
+}
+
+///
 abstract class Entity<T extends ValueObject> implements Identifiable<T> {
   const Entity(this.id);
 
