7126c296a20116c5d819d38041cf5472d4dfb76b
Andres Atehortua <andres.atehortua@zemoga.com>
2022-05-19 16:31:03 -0500

    Further cache extensions improvements.

--- lib/src/cache/cache.dart
+++ lib/src/cache/cache.dart
@@ -22,9 +22,40 @@ class Cache<T> {
 }
 
 ///
-extension ListCacheExt<T> on Cache<List<T>> {
-  void mutateList(void Function(List<T> data) block) {
+extension ListCacheExt<E> on Cache<List<E>> {
+  Stream<List<E>> valuesWhere(bool Function(E value) test) {
+    return stream.map((event) => event.where(test).toList());
+  }
+
+  Stream<E> find(
+    bool Function(E value) test, {
+    required E Function() orElse,
+  }) {
+    return stream.map((event) => event.firstWhere(test, orElse: orElse));
+  }
+
+  void update(void Function(List<E> data) block) {
     data = data.toList().also(block);
+  }
+
+  void add(E value) {
+    update((data) => data.add(value));
+  }
+
+  void addAll(List<E> values) {
+    update((data) => data.addAll(values));
+  }
+
+  void replaceAll(List<E> values) {
+    data = List.of(values);
+  }
+
+  void removeWhere(bool Function(E value) test) {
+    update((data) => data.removeWhere(test));
+  }
+
+  void clear() {
+    data = [];
   }
 }
 
@@ -32,56 +63,51 @@ extension ListCacheExt<T> on Cache<List<T>> {
 extension MapCacheExt<K, V> on Cache<Map<K, V>> {
   Stream<List<V>> get values => stream.map((event) => event.values.toList());
 
-  Stream<List<V>> valuesWhere(
-    bool Function(V value) test,
-  ) {
+  Stream<List<V>> valuesWhere(bool Function(V value) test) {
     return values.map((event) => event.where(test).toList());
   }
 
-  Stream<V?> valueOrNull(String key) {
+  Stream<V?> valueOrNull(K key) {
     return stream.map((event) => event[key]);
   }
 
-  Stream<V> valueOrElse(
-    String key, {
-    required V Function() orElse,
-  }) {
+  Stream<V> valueOrElse(K key, {required V Function() orElse}) {
     return valueOrNull(key).map((event) => event ?? orElse());
   }
 
-  void mutateMap(void Function(Map<K, V> data) block) {
-    data = Map.of(data).also(block);
-  }
-}
-
-///
-extension CollectionCacheExt<T> on Cache<Map<String, T>> {
-  @Deprecated('Use data directly. This will be removed in a future version')
-  bool contains(String key) {
+  bool contains(K key) {
     return data.containsKey(key);
   }
 
-  @Deprecated('Use mutateMap instead. This will be removed in a future version')
-  void add(String key, T value) {
-    mutateMap((data) => data[key] = value);
+  void update(void Function(Map<K, V> data) block) {
+    data = Map.of(data).also(block);
   }
 
-  @Deprecated('Use mutateMap instead. This will be removed in a future version')
-  void addAll(Map<String, T> other) {
-    mutateMap((data) => data.addAll(other));
+  void put(K key, V value) {
+    update((data) => data[key] = value);
   }
 
-  @Deprecated('Use mutateMap instead. This will be removed in a future version')
-  void replaceAll(Map<String, T> other) {
+  @Deprecated('Use put instead. This will be removed in a future version')
+  void add(K key, V value) {
+    put(key, value);
+  }
+
+  void addAll(Map<K, V> other) {
+    update((data) => data.addAll(other));
+  }
+
+  void replaceAll(Map<K, V> other) {
     data = Map.of(other);
   }
 
-  @Deprecated('Use mutateMap instead. This will be removed in a future version')
   void remove(String key) {
-    mutateMap((data) => data.remove(key));
+    update((data) => data.remove(key));
   }
 
-  @Deprecated('Use mutateMap instead. This will be removed in a future version')
+  void removeWhere(bool Function(K key, V value) test) {
+    update((data) => data.removeWhere(test));
+  }
+
   void clear() {
     data = {};
   }
