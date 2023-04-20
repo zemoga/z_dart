d9262cefb8e59c2003b79cd634402b3b86300c78
Andres Atehortua <andres.atehortua@zemoga.com>
2023-04-20 16:02:37 -0500

    Improve async-value readability.

--- lib/src/async/async_value.dart
+++ lib/src/async/async_value.dart
@@ -10,36 +10,67 @@ abstract class AsyncValue<T> {
   const factory AsyncValue.error(Object error, StackTrace stackTrace) =
       AsyncValueError<T>;
 
-  R fold<R>(
-    R Function() ifLoading,
-    R Function(T data) ifData,
-    R Function(Object error, StackTrace stackTrace) ifError,
-  );
+  R fold<R>({
+    required R Function() ifLoading,
+    required R Function(T data) ifData,
+    required R Function(Object error, StackTrace stackTrace) ifError,
+  });
 
-  bool get isLoading => fold(() => true, (_) => false, (e, st) => false);
+  bool get isLoading {
+    return fold(
+      ifLoading: () => true,
+      ifData: (_) => false,
+      ifError: (e, st) => false,
+    );
+  }
 
-  bool get isData => fold(() => false, (_) => true, (e, st) => false);
+  bool get isData {
+    return fold(
+      ifLoading: () => false,
+      ifData: (_) => true,
+      ifError: (e, st) => false,
+    );
+  }
 
-  bool get isError => fold(() => false, (_) => false, (e, st) => true);
+  bool get isError {
+    return fold(
+      ifLoading: () => false,
+      ifData: (_) => false,
+      ifError: (e, st) => true,
+    );
+  }
 
-  bool isErrorOfType<E extends Exception>() =>
-      fold(() => false, (_) => false, (error, _) => error is E);
+  bool isErrorOfType<E extends Exception>() {
+    return fold(
+      ifLoading: () => false,
+      ifData: (_) => false,
+      ifError: (e, st) => e is E,
+    );
+  }
 
   T? getOrNull() {
-    return fold(() => null, (data) => data, (e, st) => null);
+    return fold(
+      ifLoading: () => null,
+      ifData: (data) => data,
+      ifError: (e, st) => null,
+    );
   }
 
   T getOrElse(T Function() dflt) {
-    return fold(dflt, (data) => data, (e, st) => dflt());
+    return fold(
+      ifLoading: dflt,
+      ifData: (data) => data,
+      ifError: (e, st) => dflt(),
+    );
   }
 
   T operator |(T dflt) => getOrElse(() => dflt);
 
   AsyncValue<R> map<R>(R Function(T t) mapper) {
     return fold(
-      AsyncValue.loading,
-      (t) => AsyncValue.data(mapper(t)),
-      AsyncValue.error,
+      ifLoading: AsyncValue.loading,
+      ifData: (t) => AsyncValue.data(mapper(t)),
+      ifError: AsyncValue.error,
     );
   }
 
@@ -48,26 +79,32 @@ abstract class AsyncValue<T> {
     void Function(T data)? isData,
     void Function(Object error, StackTrace stackTrace)? isError,
   }) {
-    fold(isLoading ?? () {}, isData ?? (d) {}, isError ?? (e, st) {});
+    fold(
+      ifLoading: isLoading ?? () {},
+      ifData: isData ?? (d) {},
+      ifError: isError ?? (e, st) {},
+    );
   }
 
   @override
-  String toString() => fold(
-        () => 'Loading',
-        (t) => 'Data($t)',
-        (e, st) => 'Error($e, $st)',
-      );
+  String toString() {
+    return fold(
+      ifLoading: () => 'Loading',
+      ifData: (data) => 'Data($data)',
+      ifError: (e, st) => 'Error($e, $st)',
+    );
+  }
 }
 
 class AsyncValueLoading<T> extends AsyncValue<T> {
   const AsyncValueLoading();
 
   @override
-  R fold<R>(
-    R Function() ifLoading,
-    R Function(T data) ifData,
-    R Function(Object error, StackTrace stackTrace) ifError,
-  ) {
+  R fold<R>({
+    required R Function() ifLoading,
+    required R Function(T data) ifData,
+    required R Function(Object error, StackTrace stackTrace) ifError,
+  }) {
     return ifLoading();
   }
 
@@ -86,11 +123,11 @@ class AsyncValueData<T> extends AsyncValue<T> {
   final T data;
 
   @override
-  R fold<R>(
-    R Function() ifLoading,
-    R Function(T data) ifData,
-    R Function(Object error, StackTrace stackTrace) ifError,
-  ) {
+  R fold<R>({
+    required R Function() ifLoading,
+    required R Function(T data) ifData,
+    required R Function(Object error, StackTrace stackTrace) ifError,
+  }) {
     return ifData(data);
   }
 
@@ -112,11 +149,11 @@ class AsyncValueError<T> extends AsyncValue<T> {
   final StackTrace stackTrace;
 
   @override
-  R fold<R>(
-    R Function() ifLoading,
-    R Function(T data) ifData,
-    R Function(Object error, StackTrace stackTrace) ifError,
-  ) {
+  R fold<R>({
+    required R Function() ifLoading,
+    required R Function(T data) ifData,
+    required R Function(Object error, StackTrace stackTrace) ifError,
+  }) {
     return ifError(error, stackTrace);
   }
 
