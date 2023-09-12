cd96b5156c1879e313a2ad28a7b1c106da5505c3
Andres Atehortua <andres.atehortua@zemoga.com>
2023-09-12 13:00:54 -0500

    Migrate z_dart to package.

--- packages/z_dart/lib/src/core/paged_list.dart
+++ packages/z_dart/lib/src/core/paged_list.dart
@@ -1,12 +1,12 @@
 part of z.dart.core;
 
 class PagedList<E> {
-  final int totalElements;
-  final List<E> elements;
-
   const PagedList(this.totalElements, this.elements);
 
   const PagedList.empty() : this(0, const []);
+
+  final int totalElements;
+  final List<E> elements;
 
   PagedList<T> map<T>(T Function(E e) mapper) => PagedList(
         totalElements,
